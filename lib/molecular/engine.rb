4010d4eb6159ae63b45603d686d4d8e859cd5716
Gabriel Oliveira <oliveira.gabriel07@gmail.com>
2015-11-13 20:34:51 -0200

    Use main app layout by default
    Signed-off-by: Gabriel Oliveira <oliveira.gabriel07@gmail.com>

--- lib/molecular/engine.rb
+++ lib/molecular/engine.rb
@@ -2,10 +2,6 @@ module Molecular
   class Engine < ::Rails::Engine
     isolate_namespace Molecular
 
-    # Engine.routes.draw do
-    #   resources :campaigns
-    # end
-
     config.generators do |g|
       g.test_framework :rspec, fixture: false
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
