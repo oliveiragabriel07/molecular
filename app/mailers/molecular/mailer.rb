9a8e6bf7f4bba7ddab904848452bfe3a2b6b873a
Gabriel Oliveira <gabriel.oliveira@unicv.com.br>
2015-11-30 18:12:31 -0200

    Add custom mandrill metadata
    Add custom parameter to identify list on WebHooks
    
    Signed-off-by: Gabriel Oliveira <gabriel.oliveira@unicv.com.br>

--- app/mailers/molecular/mailer.rb
+++ app/mailers/molecular/mailer.rb
@@ -3,12 +3,14 @@ module Molecular
   class Mailer < ActionMailer::Base
     default from: 'from@example.com'
 
-    def campaign_email(campaign, recipient)
-      mail(to: recipient.email,
+    def campaign_email(campaign, list)
+      mail(to: list.recipient.email,
            subject: campaign.subject) do |format|
         format.text { render plain: campaign.body }
         format.html { render html: campaign.body.html_safe }
       end
+
+      headers['X-MC-Metadata'] = {list_id: list.id}.to_json
     end
   end
 end
