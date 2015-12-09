8ee12cb391cde0cc587899cc618f2b37cfe6dbe3
Gabriel Oliveira <gabriel.oliveira@unicv.com.br>
2015-12-09 14:42:59 -0200

    Mount resque on dummy
    Signed-off-by: Gabriel Oliveira <gabriel.oliveira@unicv.com.br>

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,6 +1,10 @@
+require 'resque/server'
+
 Rails.application.routes.draw do
   get 'users', to: 'user#index', as: :molecular_recipients
 
   mount_molecular campaigns: 'custom_molecular/campaigns'
+  mount Resque::Server, at: "/resque"
+
   resource :attachments, only: [:new, :create]
 end
