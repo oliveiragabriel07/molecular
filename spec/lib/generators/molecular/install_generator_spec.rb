a7233db27f9c257f0ad6e36776bb2eee8c1eebab
Gabriel Oliveira <gabriel.oliveira@unicv.com.br>
2015-12-09 23:36:31 -0200

    Rspec install generator
    Signed-off-by: Gabriel Oliveira <gabriel.oliveira@unicv.com.br>

--- spec/lib/generators/molecular/install_generator_spec.rb
+++ spec/lib/generators/molecular/install_generator_spec.rb
@@ -6,33 +6,50 @@ module Molecular
   describe InstallGenerator do
     destination File.expand_path("../../../../tmp", __FILE__)
 
-    before(:all) do
-      prepare_destination
-      run_generator
+    context 'creates' do
+      before(:all) do
+        prepare_destination
+        run_generator
+      end
+
+      it "a molecular initializer" do
+        assert_file "config/initializers/molecular.rb"
+      end
+
+      it "molecular campaign migration" do
+        assert_migration "db/migrate/create_molecular_campaigns.rb"
+      end
+
+      it "molecular events migration" do
+        assert_migration "db/migrate/create_molecular_events.rb"
+      end
+
+      it "molecular recipients migration" do
+        assert_migration "db/migrate/create_molecular_recipients.rb"
+      end
+
+      it "molecular subscriptions migration" do
+        assert_migration "db/migrate/create_molecular_subscriptions.rb"
+      end
+
+      it "molecular campaign decorator" do
+        assert_file "app/decorators/models/molecular/campaign_decorator.rb"
+      end
     end
 
-    it "creates a molecular initializer" do
-      assert_file "config/initializers/molecular.rb"
-    end
+    # it "skips existing migrations" do
+    #   prepare_destination
 
-    it "creates molecular campaign migration" do
-      assert_migration "db/migrate/create_molecular_campaigns.rb"
-    end
+    #   content = "content wont be changed"
+    #   ts = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
+    #   migration = "db/migrate/#{ts}_create_molecular_subscriptions.rb"
+    #   path = File.join(test_case.destination_root, migration)
 
-    it "creates molecular events migration" do
-      assert_migration "db/migrate/create_molecular_events.rb"
-    end
+    #   FileUtils.mkdir_p(File.dirname(path))
+    #   File.open(path, "w+") { |f| f.write(content) }
 
-    it "creates molecular recipients migration" do
-      assert_migration "db/migrate/create_molecular_recipients.rb"
-    end
-
-    it "creates molecular subscriptions migration" do
-      assert_migration "db/migrate/create_molecular_subscriptions.rb"
-    end
-
-    it "creates molecular campaign decorator" do
-      assert_file "app/decorators/models/molecular/campaign_decorator.rb"
-    end
+    #   run_generator
+    #   assert_file migration, content
+    # end
   end
 end
