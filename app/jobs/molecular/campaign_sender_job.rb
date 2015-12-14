fe8acbe88bacb23612dbb5a6f83ec0e9f0b3ea62
Gabriel Oliveira <gabriel.oliveira@unicv.com.br>
2015-12-14 19:38:23 -0200

    CampaignSender: update campaign#sent_at only if new
    Signed-off-by: Gabriel Oliveira <gabriel.oliveira@unicv.com.br>

--- app/jobs/molecular/campaign_sender_job.rb
+++ app/jobs/molecular/campaign_sender_job.rb
@@ -3,15 +3,15 @@ module Molecular
     queue_as :molecular_campaigns
 
     def perform(campaign)
+      # update campaing status
+      campaign.update(sent_at: Time.zone.now) unless campaign.sent_at.present?
+
       campaign.recipients.find_each do |u|
         recipient = Molecular::Recipient.find_or_create_by(email: u.email)
 
         subscription = campaign.subscribe(recipient)
         subscription.try(:deliver)
       end
-
-      # update campaing status
-      campaign.update(sent_at: Time.zone.now)
     end
   end
 end
