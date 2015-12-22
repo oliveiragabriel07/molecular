3abe0f56dda689c15e9b2744d8efbc0447e202ca
Gabriel Oliveira <gabriel.oliveira@unicv.com.br>
2015-12-22 16:49:34 -0200

    Begin engine Localization
    Signed-off-by: Gabriel Oliveira <gabriel.oliveira@unicv.com.br>

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
-require "molecular"
+require 'molecular'
 
 module Dummy
   class Application < Rails::Application
@@ -18,6 +18,7 @@ module Dummy
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.default_locale = :'pt-BR'
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
